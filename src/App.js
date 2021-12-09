a45c8cae11c0650508879614506e4007040ebbcd
Muhammad Inaam Ur Rehman <inaamurrehman343@gmail.com>
2021-12-09 10:44:18 +0500

    secnff

--- src/App.js
+++ src/App.js
@@ -6,24 +6,76 @@ import Result from "./components/Result";
 
 const App = () => {
   const [data, setData] = useState("");
+  const [result, setResult] = useState("");
+  const [firstNumber, setFirstNumber] = useState(0);
+  const [operator, setOperator] = useState("");
 
   function handleNumericButton(e) {
-    console.log(e.target.value);
-    setData(data + e.target.value)
+    setResult(0);
+    let val = e.target.value;
+    setData(data + e.target.value);
+    if (val === "+") {
+      setFirstNumber(parseInt(data));
+      setOperator(val);
+      console.log(operator);
+      setData("");
+    }
+    if (val === "-") {
+      setFirstNumber(parseInt(data));
+      setOperator(val);
+      setData("");
+    }
+    if (val === "x") {
+      setFirstNumber(parseInt(data));
+      setOperator(val);
+      setData("");
+    }
+    if (val === "÷") {
+      setFirstNumber(parseInt(data));
+      setOperator(val);
+      setData("");
+    }
+    if (val === "=") {
+      let op = operator;
+      console.log(op);
+      if (op === "+") {
+        const d = firstNumber + parseInt(data);
+        setResult(d);
+      }
+      if (op === "-") {
+        const d = firstNumber - parseInt(data);
+        setResult(d);
+      }
+      if (op === "x") {
+        const d = firstNumber * parseInt(data);
+        setResult(d);
+      }
+      if (op === "÷") {
+        const d = firstNumber / parseInt(data);
+        setResult(d);
+      }
+      setData("");
+
+
+    }
   }
   function onClear() {
     setData("");
+    setResult(0);
+  }
+  function onClickCE() {
+    setData(data.slice(0, -1))
   }
   return (
     <div className="app">
-      <Result data={data} />
+      <Result data={data} result={result} />
       <div className="keypad">
         <div className="keypad__inner">
           <button onClick={(e) => { handleNumericButton(e) }} value="(">(</button>
-          <button onClick={(e) => { handleNumericButton(e) }} value="CE">CE</button>
+          <button onClick={onClickCE} value="CE">CE</button>
           <button onClick={(e) => { handleNumericButton(e) }} value=")">)</button>
           <button onClick={onClear} value="C">C</button>
-          <button onClick={(e) => { handleNumericButton(e) }} value="1">1</button>
+          <button onClick={(e) => { handleNumericButton(e) }} value={1}>1</button>
           <button onClick={(e) => { handleNumericButton(e) }} value="2">2</button>
           <button onClick={(e) => { handleNumericButton(e) }} value="3">3</button>
           <button onClick={(e) => { handleNumericButton(e) }} value="+">+</button>
@@ -38,7 +90,7 @@ const App = () => {
           <button onClick={(e) => { handleNumericButton(e) }} value=".">.</button>
           <button onClick={(e) => { handleNumericButton(e) }} value="0">0</button>
           <button onClick={(e) => { handleNumericButton(e) }} value="="> = </button>
-          <button onClick={(e) => { handleNumericButton(e) }} value="/">÷</button>
+          <button onClick={(e) => { handleNumericButton(e) }} value="÷">÷</button>
         </div>
       </div>
     </div>
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./result.css"
 
-function Result({ data }) {
+function Result({ data, result }) {
     return (
         <div className="result">
             <p>{data}</p>
+            <p>{result ? result : ""}</p>
         </div>
     )
 }
